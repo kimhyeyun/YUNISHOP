3178d043763be452e3b3ff5723a1fc42cf77e3cd
Kimhyeyun <cqqwer@gmail.com>
2022-11-21 21:56:12 +0900

    Feat: 회원 가입 시 비밀번호 암호화

--- src/main/java/com/example/yunishop/web/controller/MemberController.java
+++ src/main/java/com/example/yunishop/web/controller/MemberController.java
@@ -3,12 +3,16 @@ package com.example.yunishop.web.controller;
 import com.example.yunishop.service.MemberService;
 import com.example.yunishop.web.dto.MemberFormDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
     public String memberJoinForm() {
@@ -18,6 +22,8 @@ public class MemberController {
     @PostMapping(value = "/new")
     @ResponseBody
     public Long join(@RequestBody MemberFormDto memberFormDto) {
+        String encodePassword = passwordEncoder.encode(memberFormDto.getPassword());
+        memberFormDto.setPassword(encodePassword);
 
         return memberService.save(memberFormDto);
     }
