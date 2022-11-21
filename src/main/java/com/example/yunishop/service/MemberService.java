546faa531cc0292b3734be03bbb53a13c737f203
Kimhyeyun <cqqwer@gmail.com>
2022-11-21 21:43:57 +0900

    Feat: findById() 메서드 생성

--- src/main/java/com/example/yunishop/service/MemberService.java
+++ src/main/java/com/example/yunishop/service/MemberService.java
@@ -1,11 +1,13 @@
 package com.example.yunishop.service;
 
+import com.example.yunishop.domain.member.Member;
 import com.example.yunishop.domain.member.MemberRepository;
 import com.example.yunishop.web.dto.MemberFormDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +17,9 @@ public class MemberService {
 
     public Long save(MemberFormDto memberFormDto) {
         return memberRepository.save(memberFormDto.toEntity()).getId();
+    }
+
+    public Optional<Member> findById(Long id) {
+        return memberRepository.findById(id);
     }
 }
