136044070e9722fa7ee05ca7b309acfaca5fe612
Kimhyeyun <cqqwer@gmail.com>
2022-11-21 21:31:43 +0900

    Feat: 회원가입 화면에서 넘어오는 정보를 저장할 객체

---
+++ src/main/java/com/example/yunishop/web/dto/MemberFormDto.java
@@ -0,0 +1,47 @@
+package com.example.yunishop.web.dto;
+
+import com.example.yunishop.domain.member.Member;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+@NoArgsConstructor
+public class MemberFormDto {
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+    @NotEmpty(message = "아이디는 필수 입력 값입니다.")
+    private String memberId;
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
+    private String password;
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
+    private int postCode;
+    private String address;
+    private String additionalAddress;
+
+    public MemberFormDto(String memberId, String password, int postCode, String address, String additionalAddress) {
+        this.memberId = memberId;
+        this.password = password;
+        this.postCode = postCode;
+        this.address = address;
+        this.additionalAddress = additionalAddress;
+    }
+
+    @Builder
+    public Member toEntity() {
+        return Member.builder()
+                .name(name)
+                .memberId(memberId)
+                .password(password)
+                .postCode(postCode)
+                .address(address)
+                .addtionalAddress(additionalAddress)
+                .build();
+    }
+}
